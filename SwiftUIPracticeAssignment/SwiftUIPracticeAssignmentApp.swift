d1423ba9d92d24c1454e252cde65169b44940331
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-04-19 03:04:27 +0900

    Initial Commit

---
+++ SwiftUIPracticeAssignment/SwiftUIPracticeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeAssignmentApp.swift
+//  SwiftUIPracticeAssignment
+//
+//  Created by hwijinjeong on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
